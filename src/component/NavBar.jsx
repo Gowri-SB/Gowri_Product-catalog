ad9d99bb0006c09dcf05ab5fefa60972c64085c7
Gowri-SB <manishagowri72@gmail.com>
2025-08-29 14:00:42 +0530

    updated

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,7 +3,7 @@ function NavBar() {
   return (
    <div>
     <center>
-  <h1 className="mt-5 text-blue-600 font-extrabold text-3xl">Product Catalog</h1>
+  <h1 className="mt-5 text-black-600 font-extrabold text-3xl">Product Catalog</h1>
     <h3>Discover amazing products</h3>
     </center>
       </div>
